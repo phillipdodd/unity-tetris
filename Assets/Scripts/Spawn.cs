390483beee0e16439282f2dbd753d5e0ffda64db
Phillip Dodd <phillipdodd@gmail.com>
2020-07-23 21:55:42 -0400

    init

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject[] groups;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnNext();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnNext()
+    {
+        int i = Random.Range(0, groups.Length);
+        Instantiate(
+            groups[i], 
+            transform.position, 
+            Quaternion.identity);
+    }
+
+}
