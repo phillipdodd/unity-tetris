52783197a71aec742309de27c871651aa1c62edd
Phillip Dodd <phillipdodd@gmail.com>
2020-07-25 14:55:04 -0400

    not sure

--- Assets/Scripts/Group.cs
+++ Assets/Scripts/Group.cs
@@ -17,7 +17,7 @@ public class Group : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
